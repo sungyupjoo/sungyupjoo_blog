9e7e6203dcaf72d073fe42abcb9c261e0cf396b0
sungyupjoo <chickenjuicyham@gmail.com>
2024-10-14 15:34:30 +0900

    added basic home page, globals css

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet, </h1>
+      <p>
+        consectetur adipisicing elit. Voluptatibus perferendis veritatis sequi
+        quia temporibus. Natus ad dolores beatae possimus in harum distinctio
+        doloremque eveniet, molestias delectus porro eos vitae magni!
+        consectetur adipisicing elit. Voluptatibus perferendis veritatis sequi
+        quia temporibus. Natus ad dolores beatae possimus in harum distinctio
+        doloremque eveniet, molestias delectus porro eos vitae magni!consectetur
+        adipisicing elit. Voluptatibus perferendis veritatis sequi quia
+        temporibus. Natus ad dolores beatae possimus in harum distinctio
+        doloremque eveniet, molestias delectus porro eos vitae magni! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Expedita perferendis
+        mollitia, corrupti quasi, nisi incidunt, perspiciatis provident
+        blanditiis excepturi enim praesentium quibusdam omnis sapiente quos?
+        Quod autem magnam nostrum excepturi?
+      </p>
+    </div>
+  );
+}
