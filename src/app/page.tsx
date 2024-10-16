2457859331c429a77b6ef2b3cdc152a2af218538
sungyupjoo <chickenjuicyham@gmail.com>
2024-10-16 10:29:00 +0900

    added featured section temporarily.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Featured from "@/components/featured/Featured";
 import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Featured />
+    </div>
+  );
 }
--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
+import p1 from "@/assets/images/p1.jpeg";
 
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        안녕하세요,
+        <br />
+        주성엽의 블로그입니다
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src={p1} alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet</h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quos.
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
